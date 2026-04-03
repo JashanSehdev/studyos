ad1c7961f3b252874ac3be002b921b1cc6e49225
Jashan Sehdev <sehdevjashan321@gmail.com>
2026-04-03 07:26:09 +0530

    final confugartions has been done, studyos is ready for production

--- server/db.js
+++ server/db.js
@@ -18,7 +18,8 @@ const pool = new Pool(
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port:     process.env.DB_PORT,
-      }
+      }    
+  
 )
 
 export default pool;
